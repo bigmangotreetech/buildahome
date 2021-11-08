4063abc2915aa31d6d6bffa85b5661f1b3e51c2f
aravindathreya <aravind.capricon@gmail.com>
2021-11-08 14:37:57 +0530

    Fix for style.css 404

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from time import mktime
 import os
 import hashlib
 import time
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 # Sql setup
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'buildahome'
