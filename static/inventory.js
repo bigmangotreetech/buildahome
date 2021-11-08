dd8d1ac55d6d457e5b0e2043374fdf315b5c2ad2
aravindathreya <aravind.capricon@gmail.com>
2021-11-08 16:32:15 +0530

    Added links to navbar

--- static/inventory.js
+++ static/inventory.js
@@ -1,4 +1,5 @@
 $('#get_procurement').on('click', function(){
     const project = $("#project").val()
+    if(project.length)
     window.location.href = '/material/view_inventory?project_id='+project.toString()
 })
