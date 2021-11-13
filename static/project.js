5f1c4ffc6f8ca06ebd7f759656f1f521af779f0b
aravindathreya <aravind.capricon@gmail.com>
2021-11-13 14:09:32 +0530

    UPdate trades ajax call patch 5

--- static/project.js
+++ static/project.js
@@ -10,10 +10,13 @@ $(document).ready(function() {
             $.ajax({
               url: '/material/update_trades_for_project',
               type: "POST",
+              dataType: 'json'
               data: {'project_id': project_id},
               success: function(data){
-
-                  for(const trade of data) {
+                  console.log(data)
+                  const trades = JSON.parse(data)
+                  console.log(trades)
+                  for(const trade of trades) {
                     $(".select_trade_for_bill select").append(`${trade}`)
                   }
               }
