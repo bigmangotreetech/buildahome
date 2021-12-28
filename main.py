6a1f2109184e8db34b837d0d188d6c6a6369ced8
aravindathreya <aravind.capricon@gmail.com>
2021-12-28 12:13:56 +0530

    PO upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session, flash, jsonify, send_from_directory, make_response
+from flask import Flask, render_template, redirect, request, session, flash, jsonify, url_for
 from flask_mysqldb import MySQL
 import hashlib
 
@@ -7,18 +7,26 @@ import time
 from time import mktime
 import os
 import time
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 # Sql setup
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'buildahome'
 app.config['MYSQL_PASSWORD'] = 'build*2019'
 app.config['MYSQL_DB'] = 'buildahome2016'
-app.config['UPLOAD_FOLDER'] = 'images'
+app.config['UPLOAD_FOLDER'] = 'files'
 app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
 
 mysql = MySQL(app)
 
+
 app.secret_key = b'bAhSessionKey'
+ALLOWED_EXTENSIONS = ['pdf']
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_projects():
     cur = mysql.connection.cursor()
@@ -491,6 +499,24 @@ def view_indent_details():
         cur.execute(indents_query, (indent_id))
         result = cur.fetchone()
         return render_template('view_indent_details.html', result=result)
+
+
+@app.route('/upload_po_for_indent', methods=['POST'])
+def upload_po_for_indent():
+    if 'email' not in session:
+        flash('You need to login to continue', 'danger')
+        session['last_route'] = '/material/create_bill'
+        return redirect('/material/login')
+    if request.method == 'POST':
+        if 'purchase_order' in request.files:
+            file = request.files['purchase_order']
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('download_file', name=filename))
 
 @app.route('/logout', methods=['GET'])
 def logout():
