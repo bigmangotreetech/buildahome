81759fe0e71668345b3552bf435fbb6cc7f24390
aravindathreya <aravind.capricon@gmail.com>
2022-01-08 14:33:43 +0530

    PO Upload notification 1

--- main.py
+++ main.py
@@ -526,8 +526,8 @@ def upload_po_for_indent():
                 mysql.connection.commit()
 
                 get_indent_query = 'SELECT indents.id, projects.project_id, projects.project_name, indents.material, indents.quantity, indents.unit, indents.purpose' \
-                                ', App_users.name, indents.timestamp, indents.created_by_user, indents.acted_by_user FROM indents INNER JOIN projects on indents.status="unapproved" AND indents.project_id=projects.project_id ' \
-                                ' LEFT OUTER JOIN App_users on indents.created_by_user=App_users.user_id AND indents.id='+str(indent_id)
+                                ', App_users.name, indents.timestamp, indents.created_by_user, indents.acted_by_user FROM indents INNER JOIN projects on indents.project_id=projects.project_id ' \
+                                ' AND indents.id='+str(indent_id)
                 cur.execute(get_indent_query)
                 result = cur.fetchone()
                 if result is not None:
