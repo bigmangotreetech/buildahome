59dd68993c8db31cab93185a91070ecfd083ea7d
aravindathreya <aravind.capricon@gmail.com>
2022-01-17 22:06:35 +0530

    Create project get and post 6

--- main.py
+++ main.py
@@ -604,6 +604,7 @@ def create_project():
 
         cur = mysql.connection.cursor()
         new_project_query = 'INSERT into projects'+str(tuple(column_names)).replace("'","")+'values '+str(tuple(values))
+        return new_project_query + str(tuple(values))
         cur.execute(new_project_query, tuple(values))
         project_id = cur.lastrowid
         if 'cost_sheet' in request.files:
