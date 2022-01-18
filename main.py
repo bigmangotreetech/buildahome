abf32722e326c792c875dbf4c9a457a7d85fdec2
aravindathreya <aravind.capricon@gmail.com>
2022-01-18 10:21:12 +0530

    Edit project 5

--- main.py
+++ main.py
@@ -649,6 +649,7 @@ def edit_project():
         # Remove the last comma
         update_string = update_string[:-1]
         update_project_query = 'UPDATE projects SET '+update_string+' WHERE project_id='+str(request.form['project_id'])
+        return  update_project_query
         cur = mysql.connection.cursor()
         cur.execute(update_project_query)
         flash('Project updated successfully','success')
