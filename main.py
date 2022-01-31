c20add43fee508d20f4b47d524515c335236ea41
aravindathreya <aravind.capricon@gmail.com>
2022-01-31 19:03:10 +0530

    Fixing drawings 6

--- main.py
+++ main.py
@@ -1496,10 +1496,12 @@ def drawings():
     if session['role'] not in ['Super Admin', 'COO', 'QS Head','Purchase Head', 'Site Engineer','Design Head']:
         drawings_info = "SELECT " + query_string + " FROM projects p LEFT OUTER JOIN " + table_name + " d on " \
                               "p.project_id=d.project_id AND p.is_approved=1" \
-                                  'AND p.project_id IN ' + str(session['projects'])
+                                  'WHERE p.project_id IN ' + str(session['projects'])
     else:
-        drawings_info = "SELECT "+ query_string +" FROM projects p LEFT INNER JOIN "+table_name+" d on " \
-                    "p.project_id=d.project_id AND p.is_approved=1"
+        drawings_info = "SELECT "+ query_string +" FROM projects p LEFT OUTER JOIN "+table_name+" d on " \
+                    "p.project_id=d.project_id WHERE p.is_approved=1"
+
+
 
     cur.execute(drawings_info)
     drawings = cur.fetchall()
