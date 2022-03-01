5df542761234930ccaccdf40d42afcd09f80f8e3
aravindathreya <aravind.capricon@gmail.com>
2022-03-01 13:30:52 +0530

    Showing cost sheet and site inspection report on edit project page

--- constants/constants.py
+++ constants/constants.py
@@ -2,7 +2,7 @@
 project_fields = ['project_number', 'project_name','package_type', 'project_location', 'no_of_floors', 'project_value', 'date_of_initial_advance',
             'date_of_agreement', 'sales_executive', 'site_area','basement_slab_area',
             'gf_slab_area', 'ff_slab_area','sf_slab_area','tf_slab_area', 'tef_slab_area', 'shr_oht','additional_cost' ,'elevation_details',
-            'paid_percentage', 'comments', 'is_approved']
+            'paid_percentage', 'comments', 'is_approved', 'cost_sheet', 'site_inspection_report']
 roles = [
             'QS Head',
             'QS Engineer',
