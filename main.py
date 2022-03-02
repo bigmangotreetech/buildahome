d26af3e5c34e19aeff345d3a530b939cd0d5b13f
aravindathreya <aravind.capricon@gmail.com>
2022-03-02 11:06:15 +0530

    Trying file upload to aws s3

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.config['MYSQL_DB'] = 'buildahome2016'
 app.config['UPLOAD_FOLDER'] = '../static/files'
 app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
 app.config['S3_SECRET'] = 'RWBMkQ5UOeAUbg3GZmLb5EOq01rXfKUz+aIS4xvG'
-app.config['S3_KEY'] = 'AKIAJIK4Q2RPVOLL7PKQ'
+app.config['S3_KEY'] = 'AKIA25KGDJYARPIVQ763'
 app.config['S3_BUCKET'] = 'buildahomeerp'
 
 mysql = MySQL(app)
