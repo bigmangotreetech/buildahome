fa70908f9b35aa4a32386ac9e2377817f7eb021b
aravindathreya <aravind.capricon@gmail.com>
2022-03-07 18:23:23 +0530

    Fixing view project when there is no sales executive

--- main.py
+++ main.py
@@ -1707,6 +1707,7 @@ def view_project_details():
         cur.execute(get_details_query)
         result = cur.fetchone()
         details = {}
+        return jsonify(result)
 
 
         sales_executive_query = 'SELECT name from App_users WHERE user_id='+str(result[8])
