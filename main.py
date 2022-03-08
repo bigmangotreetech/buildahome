643e3ce29b87030931ec7706a738dd0f14702a44
aravindathreya <aravind.capricon@gmail.com>
2022-03-08 07:23:14 +0530

    Fixing view contractor info page

--- main.py
+++ main.py
@@ -1086,8 +1086,8 @@ def project_contractor_info():
     get_wo_query = 'SELECT c.name, c.code, c.pan, ' \
                    'w.value, w.balance, b.trade,  b.stage, b.payment_percentage, b.amount, b.approval_2_amount' \
                    ' FROM work_orders w INNER JOIN wo_bills b on ' \
-                   'w.contractor_name=b.contractor_name AND w.contractor_code = b.contractor_code AND ' \
-                   'w.project_id=%s AND w.contractor_name=%s AND w.contractor_code=%s AND ' \
+                   'c.name=b.contractor_name AND c.code = b.contractor_code AND ' \
+                   'w.project_id=%s AND c.name=%s AND c.code=%s AND ' \
                    'b.approval_2_amount IS NOT NULL INNER JOIN contractors c on wo.contractor_id=c.id ORDER BY w.trade'
     cur.execute(get_wo_query, (project_id, contractor_name, contractor_code))
     bills = cur.fetchall()
