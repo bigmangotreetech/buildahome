a0eef3dc1990d85428a8ffc57e3814bdcdf0e486
aravindathreya <aravind.capricon@gmail.com>
2022-03-26 18:56:02 +0530

    Type casting amount to int

--- main.py
+++ main.py
@@ -1084,8 +1084,6 @@ def export_bills():
     font.bold = True
     style.font = font
 
-    accountancy_format = xlwt.XFStyle()
-    accountancy_format.num_format_str = 'general'
 
     ws.write(1, 0, 'Contractor Name', style=style)
     ws.write(1, 1, 'Contractor PAN', style=style)
