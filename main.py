b9f0512ce7cd9774153bf30c32da1074b11cf895
aravindathreya <aravind.capricon@gmail.com>
2022-03-26 18:39:56 +0530

    Fixing approved bills

--- main.py
+++ main.py
@@ -1085,7 +1085,7 @@ def export_bills():
     style.font = font
 
     accountancy_format = xlwt.XFStyle()
-    accountancy_format.num_format_str = '0'
+    accountancy_format.num_format_str = '000000000000'
 
     ws.write(1, 0, 'Contractor Name', style=style)
     ws.write(1, 1, 'Contractor PAN', style=style)
