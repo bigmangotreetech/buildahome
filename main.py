d9df2e9891cc7663f47cd6f174a41d92ae2f5e54
aravindathreya <aravind.capricon@gmail.com>
2022-03-31 16:11:22 +0530

    Fixing unapproved work orders

--- main.py
+++ main.py
@@ -1596,7 +1596,7 @@ def view_unapproved_work_order():
                 'project_number': i[1],
                 'id': i[2],
                 'trade': i[3],
-                'value': str(int(i[4].replace(',',''))),
+                'value': str(int(i[4].strip().replace(',',''))),
                 'contractor_name': i[5],
 
             })
