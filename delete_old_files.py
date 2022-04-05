a905480123573e229788d2117049d52121eae30b
aravindathreya <aravind.capricon@gmail.com>
2022-04-05 16:48:56 +0530

    Migration

--- delete_old_files.py
+++ delete_old_files.py
@@ -5,7 +5,7 @@ BASE_DIR = '/home/buildahome2016/public_html'
 abs_path = os.path.join(BASE_DIR, '/home/buildahome2016/public_html/app.buildahome.in/api/images')
 files = os.listdir(abs_path)
 try:
-    for x in range(11322, len(files)):
+    for x in range(0, len(files)):
         i = files[x]
         print('Deleted file '+str(x))
         os.remove('/home/buildahome2016/public_html/app.buildahome.in/api/images/' + i)
