776fc772676cc34758473bcd669a3564c5f190b4
aravindathreya <aravind.capricon@gmail.com>
2022-04-13 01:36:11 +0530

    Project contractor info fixes

--- main.py
+++ main.py
@@ -1563,7 +1563,7 @@ def project_contractor_info():
     cur = mysql.connection.cursor()
     data = {'name': '', 'code': '', 'pan': '', 'value': '', 'balance': '', 'trade': '', 'contractor_id': ''}
 
-    get_contractor_query = 'SELECT id, name, code, pan from contractors code=%s'
+    get_contractor_query = 'SELECT id, name, code, pan from contractors WHERE code=%s'
     cur.execute(get_contractor_query, (contractor_code))
     res = cur.fetchone()
     if res is not None:
