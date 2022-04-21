8fdb191403f1242894a42f4caa88d78cca564551
aravindathreya <aravind.capricon@gmail.com>
2022-04-21 21:03:32 +0530

    Drawing request api

--- main.py
+++ main.py
@@ -2262,7 +2262,7 @@ def delete_indent():
         indent_id = request.args['indent_id'] 
         cur = mysql.connection.cursor()
         query = 'DELETE from indents WHERE id='+str(indent_id)
-        cur.exceute(query)
+        cur.execute(query)
         mysql.connection.commit()
         flash('Indent deleted','danger')
         return redirect('/erp/view_qs_approval_indents')
