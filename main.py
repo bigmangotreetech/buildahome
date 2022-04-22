f0b6b21a1ecb1c6200e523d1ca0111c07a546988
aravindathreya <aravind.capricon@gmail.com>
2022-04-22 20:17:25 +0530

    Added date for work order

--- main.py
+++ main.py
@@ -2326,7 +2326,7 @@ def sign_wo():
     if request.method == 'GET':
         if 'wo_id' in request.args:
             work_order_query = 'SELECT p.project_name, p.project_number, wo.trade, wo.value, c.name,' \
-                               'c.pan, c.code, c.address, wo.wo_number, wo.cheque_no, wo.comments' \
+                               'c.pan, c.code, c.address, wo.wo_number, wo.cheque_no, wo.comments, wo.created_at' \
                                ' FROM work_orders wo ' \
                                'INNER JOIN projects p on p.project_id=wo.project_id AND wo.signed=0 AND wo.id=' + str(
                 request.args['wo_id']) + ' INNER JOIN contractors c on c.id=wo.contractor_id'
@@ -2362,7 +2362,7 @@ def approve_wo():
     if request.method == 'GET':
         if 'wo_id' in request.args:
             work_order_query = 'SELECT p.project_name, p.project_number, wo.trade, wo.value, c.name,' \
-                               'c.pan, c.code, c.address, wo.wo_number, wo.cheque_no, wo.comments' \
+                               'c.pan, c.code, c.address, wo.wo_number, wo.cheque_no, wo.comments, wo.created_at' \
                                ' FROM work_orders wo ' \
                                'INNER JOIN projects p on p.project_id=wo.project_id AND wo.signed=1 AND wo.approved=0 AND wo.id=' + str(
                 request.args['wo_id']) + ' ' \
