c092aa827ba2814ed28b0ff0409fe8a290013eed
aravindathreya <aravind.capricon@gmail.com>
2022-04-29 21:11:24 +0530

    Work order for civil

--- static/project.js
+++ static/project.js
@@ -375,6 +375,9 @@ $(document).ready(function () {
             },
             success: function (data) {
                 console.log(data)
+            },
+            error: function (data) {
+                console.log(data)
             }
         });
     }
