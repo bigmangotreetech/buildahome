24c9866b2ddf3749afae391018cbe2489b84724e
aravindathreya <aravind.capricon@gmail.com>
2022-05-05 19:44:36 +0530

    Minor bug fix for file path

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ def delete_old_drawings():
         cur.execute(project_id_query)
         res = cur.fetchone()
         project_id = res[0]
-        get_drawings_for_projects = 'SELECT pdf FROM Docs WHERE project_id='+str(project_id)+'  AND (folder!="RECEIPTS" OR folder!="AGREEMENT ")'
+        get_drawings_for_projects = 'SELECT pdf FROM Docs WHERE project_id='+str(project_id)+'  AND folder!="RECEIPTS" AND folder!="AGREEMENT "'
         cur.execute(get_drawings_for_projects)
         res = cur.fetchall()
         for d in res:
@@ -205,9 +205,11 @@ def delete_old_drawings():
                 os.remove('/home/buildahome2016/public_html/app.buildahome.in/team/Drawings/'+d[0])
             except:
                 pass
-        delete_drawing_query = 'DELETE from Docs WHERE project_id='+str(project_id)+' AND (folder!="RECEIPTS" OR folder!="AGREEMENT ")'
+        delete_drawing_query = 'DELETE from Docs WHERE project_id='+str(project_id)+' AND folder!="RECEIPTS" AND folder!="AGREEMENT "'
         cur.execute(delete_drawing_query)
         break
+
+        
     
     return 'success'
 
