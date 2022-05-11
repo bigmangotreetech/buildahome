b3c1ae6f9a63b8dfea9311406c87011259c622aa
aravindathreya <aravind.capricon@gmail.com>
2022-05-11 20:42:53 +0530

    Custom work order

--- static/project.js
+++ static/project.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     $('.approve-indent-by-ph').on('click', function(e) {
         e.preventDefault()
-        url = $(this).val()
+        url = $(this).attr('href')
         difference_cost = $(".difference_cost").val().toString()
         if (difference_cost.trim() != '') {
             url += '&difference_cost='+difference_cost
