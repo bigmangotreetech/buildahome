a124e1197f609e04c32ec4a6049f3899ee7b3067
aravindathreya <aravind.capricon@gmail.com>
2022-05-25 16:07:45 +0530

    Can delete po after approved by ph

--- static/inventory.js
+++ static/inventory.js
@@ -26,10 +26,15 @@ if($('#total_item_quantity').length) {
 }
 
 if($('.item_difference_cost').length) {
+    let total_cost = 0;
+    $('.item_cost').each(function(index, element){
+        total_cost += parseFloat($(this).text().trim())
+    })
+    $("#total_cost").text(total_cost)
+
     let total_difference_cost = 0;
     $('.item_difference_cost').each(function(index, element){
         total_difference_cost += parseFloat($(this).text().trim())
     })
     $("#total_difference_cost").text(total_difference_cost)
-
 }
