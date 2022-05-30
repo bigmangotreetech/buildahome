13039a156d4da03effce7dff931076c23ff22778
aravindathreya <aravind.capricon@gmail.com>
2022-05-30 12:11:35 +0530

    Added loading and unloading cost and transportation to amount calculation

--- static/erp.js
+++ static/erp.js
@@ -2,11 +2,16 @@ function calcAmount() {
     const quantity = $('#quantity').val().trim()
     const rate = $("#rate").val().trim()
     const gst = $("#gst").val()
-    const amount = parseFloat(rate) * parseFloat(quantity)
+    const loading_unloading = $('#loading_unloading').val()
+    const transportation = $('#transportation').val()
+    const amount = parseFloat(rate) * parseFloat(quantity) 
     const total = ((gst / 100 ) * amount) + amount;
-    $("#total_amount").val(total)
+    const total_with_other_expenses = amount + parseFloat(loading_unloading) + parseFloat(transportation)
+    $("#total_amount").val(total_with_other_expenses)
 }
 
 $("#gst").on("change",  calcAmount)
 $("#rate").on("keyup", calcAmount)
 $("#quantity").on("keyup", calcAmount)
+$("#transportation").on("keyup", calcAmount)
+$("#loading_unloading").on("keyup", calcAmount)
