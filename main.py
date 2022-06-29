833289402501b1370485019a56ef6f945213bc3a
aravindathreya <aravind.capricon@gmail.com>
2022-06-29 12:29:19 +0530

    Clear balance fix

--- main.py
+++ main.py
@@ -2520,9 +2520,7 @@ def view_deleted_indents():
         flash('You need to login to continue', 'danger')
         session['last_route'] = '/erp/view_approved_POs'
         return redirect('/erp/login')
-    if session['role'] not in ['Super Admin', 'COO', 'QS Head', 'QS Engineer', 'Purchase Executive', 'Purchase Head']:
-        flash('You do not have permission to view that page', 'danger')
-        return redirect(request.referrer)
+
     if request.method == 'GET':
         cur = mysql.connection.cursor()
         current_user_role = session['role']
