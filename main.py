bd0fc47a611d9ee9d2010fcd6a910c1dd137c8bf
aravindathreya <aravind.capricon@gmail.com>
2022-07-05 11:24:39 +0530

    Updated materials list

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def get_projects():
     cur = mysql.connection.cursor()
     projects = []
     if len(get_projects_for_current_user()) > 0:
-        if session['role'] not in ['Super Admin', 'COO', 'QS Head','Purchase Head', 'Site Engineer', 'Design Head']:
+        if session['role'] not in ['Super Admin', 'COO', 'QS Head','Purchase Head', 'Site Engineer', 'Design Head','QS Info']:
             query = 'SELECT project_id, project_name from projects WHERE is_approved=1 AND archived=0 ' \
                     'AND project_id IN ' + str(get_projects_for_current_user())
             cur.execute(query)
