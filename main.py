371b55b3c784e91287f1f677d4c8b84df9fabb57
aravindathreya <aravind.capricon@gmail.com>
2022-07-07 21:51:32 +0530

    Fixing time of report

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def get_dlr_report():
     cur = mysql.connection.cursor()
     IST = pytz.timezone('Asia/Kolkata')
     current_time = datetime.now(IST)
-    current_date = current_time.strftime('%A %B %d at %H %M') 
+    current_date = current_time.strftime('%A %B %d') 
     get_projects_query = 'SELECT project_id, project_number, project_name from projects WHERE is_approved=1 AND archived=0 ORDER BY project_number'
     cur.execute(get_projects_query)
     projects = cur.fetchall()
