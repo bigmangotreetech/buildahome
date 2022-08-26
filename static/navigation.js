ea21a295334e54dd66325890ad84d8af15057cfc
aravindathreya <aravind.capricon@gmail.com>
2022-08-26 11:34:48 +0530

    Fixes 24 8

--- static/navigation.js
+++ static/navigation.js
@@ -92,6 +92,31 @@ $(document).ready(function () {
                 });
             }
         })
+
+        if($('#total_item_quantity').length) {
+            let current_quantity = 0;
+            let total_quantity = parseFloat($('#total_item_quantity').text().trim())
+            $('.item_quantity').each(function(index, element){
+                current_quantity += parseFloat($(this).text().trim())
+            })
+            let balance = total_quantity - current_quantity;
+            $('#total_current_quantity').text('Current : '+current_quantity.toString())
+            $('#total_balance_quantity').text('Balance : '+balance.toString())
+        }
+        
+        if($('.item_difference_cost').length) {
+            let total_cost = 0;
+            $('.item_cost').each(function(index, element){
+                total_cost += parseFloat($(this).text().trim())
+            })
+            $("#total_cost").text(total_cost)
+        
+            let total_difference_cost = 0;
+            $('.item_difference_cost').each(function(index, element){
+                total_difference_cost += parseFloat($(this).text().trim())
+            })
+            $("#total_difference_cost").text(total_difference_cost)
+        }
     }
 
     function initEnterMaterial() {
