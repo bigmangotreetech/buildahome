0144db0aa6bca4ef8c75777cba2f32418eeaf4c8
aravindathreya <aravind.capricon@gmail.com>
2022-09-05 21:28:16 +0530

    Fixes 24 8

--- main.py
+++ main.py
@@ -576,10 +576,11 @@ def login():
                 session['name'] = result[2]
                 session['access_level'] = result[5]
                 
-                profile_picture = '/erp/static/profile_picture.PNG'
-                if len(str(result[6]).strip()) > 0:
-                    profile_picture = result[6] 
-                session['profile_picture'] = profile_picture                 
+                # profile_picture = '/erp/static/profile_picture.PNG'
+                # if len(str(result[6])) > 0:
+                #     profile_picture = result[6] 
+                    
+                session['profile_picture'] = result[6]                 
                 
                 session['projects'] = get_projects_for_current_user()
                 flash('Logged in successfully', 'success')
