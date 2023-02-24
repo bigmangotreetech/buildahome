63efd069e71f6d1c79851b661ee803de7a5d18db
aravindathreya <aravind.capricon@gmail.com>
2023-02-24 15:56:11 +0530

    Fix

--- main.py
+++ main.py
@@ -377,8 +377,6 @@ def expenses():
                         if nt_query_res is not None:
                             try:
                                 data['total_WO_NT'] += int(float(str(nt_query_res[0]).strip().replace(',','').replace('/','').replace('\\','').replace('-','')))
-                                return str(data['total_WO_NT'])
-
                             except:
                                 return 'Error: Amount incorrect for nt bill with id '+ str(nt_query_res[1]) 
                         
